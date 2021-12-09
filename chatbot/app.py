2257c1bf889c285dc509da7b0227bb0d0df0f5f7
lohanspies <lohan.spies@didx.co.za>
2021-12-09 19:53:01 +0200

    Get docker running

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,11 +1,16 @@
 from flask import Flask  
 from flask import render_template
+from flask_cors import CORS, cross_origin
 
 # creates a Flask application, named app
 app = Flask(__name__, static_url_path='/static')
 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 # a route to display our html page gotten from [react-chat-widget](https://github.com/mrbot-ai/rasa-webchat)
 @app.route("/")
+@cross_origin()
 def index():  
     return render_template('index.html')
 
